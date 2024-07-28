cff06730798ddf493abda0bf54a0b22697724a52
chandrika <chandrikayelamasetti44@gmail.com>
2024-07-28 14:30:36 +0530

    game installation

---
+++ example.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+import random
+x=datetime.now()
+print(x)
+rock=0
+paper=1
+sissor=2
+print(f"rock={rock},paper={paper},sissor={sissor}")
+b=int(input('enter ur opinion'))
+a=random.randint(0,2)
+print(a)
+if a<b:
+    print('user win')
+elif a>b:
+    print('system wins')
+else:
+    print('tie')
